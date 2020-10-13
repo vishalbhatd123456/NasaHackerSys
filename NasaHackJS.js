e2e71cc6d37bfc99cc30c6b4c6a8ceca4546f4c2
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-10-13 22:05:07 +0530

    Create NasaHackJS.js

---
+++ NasaHackJS.js
@@ -0,0 +1,45 @@
+
+
+
+
+function whole(){
+function startup(){
+var i = 0;
+function color(){
+i = i + 1
+window.scrollTo(0, i)
+    $(function(){
+        $("body").css("backgroundColor", "#000");
+        $("body").css("color", "#0F0");
+        $("body").css("fontSize", "10pt");
+        $("body").css("fontFamily", "monospace")
+    });
+}
+setInterval(color, 25)
+    var codetorun = document.getElementById("u").innerHTML;
+    var len = codetorun.length;
+  var code=new Array(codetorun.split=(""));
+  var body = document.getElementById = ("body");
+  var x=0;
+var y=len;
+var a=0;
+var b=len;
+var z=0;
+var into;
+var p=document.getElementByTagName=("p");
+
+      
+  
+var print = function print(){
+      if(x===y){
+          }
+          
+      else{document.write(codetorun[x]);
+      x++;
+}
+ }
+  setInterval(print, 15);
+  
+}
+startup()
+}
